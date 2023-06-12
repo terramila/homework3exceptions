d87f705b204b805b016b45560606a6ac0d4e1e7c
tarana <beshirova@hotmail.com>
2023-06-13 00:30:55 +0300

    GenderException

---
+++ GenderException.java
@@ -0,0 +1,15 @@
+package homework3;
+
+public class GenderException extends Exception {
+    String data;
+
+    public GenderException(String inputData) {
+        this.data = inputData;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Неправильно указан пол: '" + data + "'!\n" +
+                "Формат ввода пола: m или f.";
+    }
+}
