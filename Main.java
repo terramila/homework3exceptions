c96fd9452be0b96647ff3bf53b787b713edb7e40
tarana <beshirova@hotmail.com>
2023-06-13 00:31:29 +0300

    Main

---
+++ Main.java
@@ -0,0 +1,8 @@
+package homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        DisplayView<View> myprog = new DisplayView<>(new DataView());
+        myprog.start();
+    }
+}
